a832fc58d633c0e05d4ae22cd0f6ccaaa3d57850
mateusz.chalik <mateusz.chalik@asseco.pl>
2025-01-02 14:37:52 +0100

    structure

---
+++ app/src/app/feature/travel/travel-details/travel-details.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-travel-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './travel-details.component.html',
+  styleUrl: './travel-details.component.scss'
+})
+export class TravelDetailsComponent {
+
+}
---
+++ app/src/app/feature/travel/travel-list/travel-list.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-travel-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './travel-list.component.html',
+  styleUrl: './travel-list.component.scss'
+})
+export class TravelListComponent {
+
+}
