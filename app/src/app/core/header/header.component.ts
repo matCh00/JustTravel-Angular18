94a7e0ff90fc5af297a418796a2fb41356897ef9
mateusz.chalik <mateusz.chalik@asseco.pl>
2025-01-09 19:20:15 +0100

    multiple planes

--- app/src/app/core/header/header.component.ts
+++ app/src/app/core/header/header.component.ts
@@ -19,14 +19,28 @@ export class HeaderComponent {
   toggleSidebar = output<boolean>();
 
 
+  planes = [
+    {src: 'plane1.png'},
+    {src: 'plane2.png'},
+    {src: 'plane3.png'},
+    {src: 'plane4.png'},
+    {src: 'plane5.png'}
+  ];
+  selectedPlane: number | null = null;
+
+
   isAnimating = false;
 
   triggerPlaneAnimation() {
     if (this.isAnimating)
       return;
+
+    this.selectedPlane = Math.floor(Math.random() * this.planes.length);
+
     this.isAnimating = true;
     setTimeout(() => {
       this.isAnimating = false;
+      this.selectedPlane = null;
     }, 6000);
   }
 
