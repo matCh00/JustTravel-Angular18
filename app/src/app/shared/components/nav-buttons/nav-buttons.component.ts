12ba34d9d00dd6f69c608725e51b10ca967ad683
mateusz.chalik <mateusz.chalik@asseco.pl>
2025-01-12 16:27:09 +0100

    better nav-buttons

--- app/src/app/shared/components/nav-buttons/nav-buttons.component.ts
+++ app/src/app/shared/components/nav-buttons/nav-buttons.component.ts
@@ -1,13 +1,16 @@
 import {AfterViewInit, Component, ElementRef, inject, input, Renderer2, viewChild} from '@angular/core';
 import {Button} from 'primeng/button';
 import {Tooltip} from 'primeng/tooltip';
+import {NgClass, NgStyle} from '@angular/common';
 
 @Component({
   selector: 'app-nav-buttons',
   standalone: true,
   imports: [
     Button,
-    Tooltip
+    Tooltip,
+    NgClass,
+    NgStyle
   ],
   templateUrl: './nav-buttons.component.html',
   styleUrl: './nav-buttons.component.scss'
@@ -16,10 +19,14 @@ export class NavButtonsComponent implements AfterViewInit {
 
   fragments = input<{frag: string, icon: string}[]>([]);
 
+  position = input<'topRight' | 'bottomCenter'>('topRight');
+
 
   navContainer = viewChild<ElementRef>('navContainer');
 
+
   private renderer: Renderer2 = inject(Renderer2);
+
 
   ngAfterViewInit(): void {
     const containerHeight = this.navContainer()!.nativeElement.offsetHeight;
