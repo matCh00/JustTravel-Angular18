afda7eeb7af7980ab83c95769e12c60c8cc9264e
mateusz.chalik <mateusz.chalik@asseco.pl>
2025-01-13 14:44:21 +0100

    instructions, styling

--- app/src/app/feature/home/instruction/instruction.component.ts
+++ app/src/app/feature/home/instruction/instruction.component.ts
@@ -1,9 +1,14 @@
 import {Component} from '@angular/core';
+import {Button} from 'primeng/button';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-instruction',
   standalone: true,
-  imports: [],
+  imports: [
+    Button,
+    RouterLink
+  ],
   templateUrl: './instruction.component.html',
   styleUrl: './instruction.component.scss'
 })
